0b475ffae8a2bf51b2663498b3fbc2edf85e8e16
Matheus <mathewcunha@gmail.com>
2026-03-18 21:08:47 -0300

    Atividade 2.1 finalizada

--- ATIVIDADE 2.1 - INTRODUÇÃO/exercicio.py
+++ ATIVIDADE 2.1 - INTRODUÇÃO/exercicio.py
@@ -24,9 +24,9 @@ t.pd()
 color = textinput('obter cor', 'escolha uma cor')
 t.begin_fill()
 t.fillcolor(color)
-t.goto(-150, 200)
-t.goto(-50, 100)
-t.goto(-250, 100)
+for _ in range(3):
+    t.fd(150)
+    t.lt(120)
 t.end_fill()
 
 t.pu()
@@ -37,11 +37,12 @@ t.begin_fill()
 t.fillcolor(color)
 t.lt(90)
 t.fd(200)
-t.goto(100, 200)
-for _ in range (2):
-    t.lt(90)
+t.rt(120)
 t.fd(100)
-t.goto(250, 100)
+for _ in range(2):
+    t.rt(60)
+    t.fd(100)
+
 t.end_fill()
 
 t.pu()
@@ -51,14 +52,15 @@ t.pd()
 color = textinput('obter cor', 'escolha uma cor')
 t.begin_fill()
 t.fillcolor(color)
-for _ in range (2):
-    t.lt(90)
+t.lt(240)
 t.fd(150)
-t.goto(50, -200)
-for _ in range (2):
-    t.lt(90)
+t.lt(60)
+t.fd(100)
+t.lt(120)
 t.fd(150)
-t.goto(250, -100)
+t.lt(60)
+t.fd(100)
+
 t.end_fill()
 
 t.pu()
@@ -68,14 +70,11 @@ t.pd()
 color = textinput('obter cor', 'escolha uma cor')
 t.begin_fill()
 t.fillcolor(color)
-t.fd(60)
-t.goto(-150, -150)
-t.goto(-190, -200)
-for _ in range (2):
-    t.lt(90)
-t.fd(60)
-t.goto(-290, -150)
-t.goto(-250, -100)
+t.rt(60)
+for _ in range(6):
+    t.fd(100)
+    t.rt(60)
+
 t.end_fill()
 
 # espiral
